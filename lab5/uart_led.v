3e4c360cd0bdb5fa6389f38ff8eb176de860c279
es-xccc <r2605572@gmail.com>
2024-02-15 00:41:05 +0800

    finish

---
+++ lab5/uart_led.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+// Date        : Wed Feb 14 00:25:33 2024
+// Host        : DESKTOP-LCJND1O running 64-bit major release  (build 9200)
+// Command     : write_verilog -mode pin_planning -force -port_diff_buffers
+//               C:/Github_Repo/xup_fpga_practice/lab5/uart_led.v
+// Design      : ios
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+module ios(led_pins, clk_pin, btn_pin, rst_pin);
+  output [7:0] led_pins;
+  input clk_pin;
+  input btn_pin;
+  input rst_pin;
+
+  // internal wires associated with differential buffers
+
+
+  // differential buffers
+
+
+endmodule
